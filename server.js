6880f92cab7b0faf31b0e4721f70b3844698731c
Jose Mejia <mejialaguna@yahoo.com>
2021-09-06 12:59:58 -0400

    add employee bug fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var inquirer = require("inquirer");
+const { query } = require("./db/connection");
 const connection = require("./db/connection");
 
 function todoList() {
@@ -48,77 +49,65 @@ function todoList() {
 
 // to add an employee
 function addEmployee() {
-  inquirer
-    .prompt([
-      {
-        message: "Employee First Name",
-        type: "input",
-        name: "firstName",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("please enter your employee First name");
-            return false;
-          }
-        },
-      },
-      {
-        message: "Employee Last Name",
-        type: "input",
-        name: "LastName",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("please your employee Last Name");
-            return false;
-          }
-        },
-      },
-      {
-        message: "Role id number",
-        type: "input",
-        name: "roleId",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            return false;
-          }
-        },
-      },
-      {
-        message: "manager id number",
-        type: "input",
-        name: "managerId",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            return false;
-          }
-        },
-      },
-    ])
-    .then((answer) => {
-      console.log(answer);
-      const sql = `INSERT INTO roles (first_name , last_name , role_id , manager_id)
-    VALUES (? , ?, ? ,?)`;
-      const params = [
-        answer.firstName,
-        answer.lastName,
-        answer.roleId,
-        answer.managerId,
-      ];
-      connection.query(sql, params, (err, result) => {
-        if (err) {
-          throw err;
-        }
-        console.table(result);
-        todoList();
+  connection.query(`select * from roles`, (err, roles) => {
+    if (err) {
+      throw error;
+    }
+    
+      let rol_title = roles.map((role) => {
+        return {
+          name: role.rol_title,
+          value: role.id,
+        };
       });
-    });
+
+      inquirer.prompt([
+        {
+          message: "Employee First Name",
+          type: "input",
+          name: "first_Name",
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log("please enter your employee First name");
+              return false;
+            }
+          },
+        },
+        {
+          message: "Employee Last Name",
+          type: "input",
+          name: "last_Name",
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log("please your employee Last Name");
+              return false;
+            }
+          },
+        },
+        {
+          message: "assing a role",
+          type: "list",
+          name: "role_Id",
+          choices: rol_title
+        },
+      ]).then((answer) => {
+        const sql = `insert into employee (first_name , last_name , role_id) 
+        values ('${answer.first_Name}' , '${answer.last_Name}' , '${answer.role_Id}' )`
+        connection.query(sql , (err, result) => {
+          if (err){
+            throw error
+          }
+          console.log("employee added");
+          console.table(result)
+          todoList()
+        })
+      })
+        
+  });
 }
 
 // TO GET ALL EMPLOYEES
@@ -140,19 +129,42 @@ function allEmployees() {
 
 // to get EMPLOYEE BY department
 function department() {
-  const sql = `SELECT employee.first_name, employee.last_name ,roles.dep_id , department.dep_name
-  FROM roles
-  JOIN department
-  ON roles.dep_id = department.id
-  JOIN employee
-  ON employee.role_id = roles.id;`;
-  connection.query(sql, (err, result) => {
+  connection.query(`SELECT * FROM department`, (err, department) => {
     if (err) {
-      throw err;
+      throw console.error();
     }
-    console.table(result);
+
+    let departmentsChoices = department.map((departments) => {
+      return {
+        name: `${departments.dep_name}`,
+      };
+    });
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "choose one of the choices",
+          name: "dep_name",
+          choices: departmentsChoices,
+        },
+      ])
+      .then((answer) => {
+        const sql = `select first_name , last_name , rol_title , salary , dep_name
+        from employee
+        join roles on(employee.role_id = roles.id)
+        join department on(department.id = roles.dep_id)
+        where dep_name = '${answer.dep_name}';
+        `;
+        connection.query(sql, (err, result) => {
+          if (err) {
+            throw err;
+          }
+          console.table(result);
+          todoList();
+        });
+      });
   });
-  todoList();
 }
 
 function deleteDepartment() {
@@ -163,9 +175,9 @@ function deleteDepartment() {
 
     let departmentChoices = department.map((departments) => {
       return {
-        name: departments.dep_name
-      }
-    })
+        name: departments.dep_name,
+      };
+    });
 
     inquirer
       .prompt([
@@ -173,7 +185,7 @@ function deleteDepartment() {
           type: "list",
           message: " which department would you like to delete",
           name: "deleteDepartment",
-          choices: departmentChoices
+          choices: departmentChoices,
         },
       ])
       .then((answer) => {
@@ -186,49 +198,48 @@ function deleteDepartment() {
           }
           console.log("department deleted");
           console.table(result);
-          todoList()
+          todoList();
         });
       });
   });
 }
 
 function deleteEmployee() {
-
-connection.query(`select * from employee` , (err , employee) => {
-  if (err){
-    throw err
-  }
-
-  let employeeChoices = employee.map((person) => {
-    return {
-      name: `${person.first_name} ${person.last_name}`,
-      value: person.id
+  connection.query(`select * from employee`, (err, employee) => {
+    if (err) {
+      throw err;
     }
-  })
-  
-  inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "which employee would like to delete",
-      name: "deleteEmployee",
-      choices : employeeChoices,
-    },
-  ])
-  .then((answer) => {
-    const sql = "DELETE FROM employee WHERE id = ?";
-    const params = `${answer.deleteEmployee}`;
-    
-    connection.query(sql, params, (err, result) => {
-      if (err) {
-        throw err;
-      }
-      console.log("Employee deleted");
-      console.table(result);
-      todoList()
+
+    let employeeChoices = employee.map((person) => {
+      return {
+        name: `${person.first_name} ${person.last_name}`,
+        value: person.id,
+      };
     });
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "which employee would like to delete",
+          name: "deleteEmployee",
+          choices: employeeChoices,
+        },
+      ])
+      .then((answer) => {
+        const sql = "DELETE FROM employee WHERE id = ?";
+        const params = `${answer.deleteEmployee}`;
+
+        connection.query(sql, params, (err, result) => {
+          if (err) {
+            throw err;
+          }
+          console.log("Employee deleted");
+          console.table(result);
+          todoList();
+        });
+      });
   });
-})
 }
 
 function deleteRole() {
@@ -238,7 +249,7 @@ function deleteRole() {
     }
     let rolesChoices = roles.map((role) => {
       return {
-        name: role.rol_title
+        name: role.rol_title,
       };
     });
     inquirer
@@ -308,7 +319,7 @@ function updateRole() {
             if (err) {
               throw err;
             }
-            console.log('role updated')
+            console.log("role updated");
             console.table(row);
             todoList();
           });
@@ -318,3 +329,71 @@ function updateRole() {
 }
 
 todoList();
+
+// inquirer
+//     .prompt([
+//       {
+//         message: "Employee First Name",
+//         type: "input",
+//         name: "first_Name",
+//         validate: (nameInput) => {
+//           if (nameInput) {
+//             return true;
+//           } else {
+//             console.log("please enter your employee First name");
+//             return false;
+//           }
+//         },
+//       },
+//       {
+//         message: "Employee Last Name",
+//         type: "input",
+//         name: "last_Name",
+//         validate: (nameInput) => {
+//           if (nameInput) {
+//             return true;
+//           } else {
+//             console.log("please your employee Last Name");
+//             return false;
+//           }
+//         },
+//       },
+//       {
+//         message: "Role id number",
+//         type: "input",
+//         name: "role_Id",
+//         validate: (nameInput) => {
+//           if (nameInput) {
+//             return true;
+//           } else {
+//             return false;
+//           }
+//         },
+//       },
+//       {
+//         message: "manager id number",
+//         type: "input",
+//         name: "manager_Id",
+//         validate: (nameInput) => {
+//           if (nameInput) {
+//             return true;
+//           } else {
+//             return false;
+//           }
+//         },
+//       },
+//     ])
+//     .then((answer) => {
+//       console.log(answer);
+//       const sql = `INSERT INTO roles (first_name , last_name , role_id , manager_id)
+//     VALUES (${answer.first_Name} , ${answer.last_Name} , ${answer.role_Id}, ${answer.manager_Id})`;
+
+//       connection.query(sql, (err, result) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log('employee added')
+//         console.table(result);
+//         todoList();
+//       });
+//     });
